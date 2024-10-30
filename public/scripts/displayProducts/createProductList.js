e9a2b6e43353768051108eac6c5da3c7992af686
kepelivi <kepelivi7@gmail.com>
2024-10-30 10:39:28 +0100

    wrap product titles in an anchor

--- public/scripts/displayProducts/createProductList.js
+++ public/scripts/displayProducts/createProductList.js
@@ -39,14 +39,22 @@ function createProduct(product) {
   img.src = product.images[0];
   img.width = 250;
   img.height = 250;
+
+  const anchor = document.createElement("a");
+  anchor.href = `details.html?productId=${product.id}`;
+  
   const title = document.createElement("h3");
   title.innerHTML = product.title;
+  anchor.appendChild(title);
+
   const desc = document.createElement("p");
   desc.innerHTML = product.description;
+
   const price = document.createElement("p");
   price.innerHTML = `Price: ${product.price}$`;
+
   div.appendChild(img);
-  div.appendChild(title);
+  div.appendChild(anchor);
   div.appendChild(desc);
   div.appendChild(price);
   return div;
